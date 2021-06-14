a472f395aaf7e36a6fc01cd8d1f3070602d7910b
qbhfh <useremail@2682147541qq.com>
2021-06-14 17:14:15 +0800

    feat:广告页倒计时跳转

--- src/js/advertisement.js
+++ src/js/advertisement.js
@@ -4,7 +4,19 @@ require('../css/advertisement.css')
 
 // console.log('advertisement');
 // 获取跳过节点
-let skipDom = document.querySelector(".skip");
-skipDom.addEventListener('click', function () {
-    location.href = './register.html';
-})
+// let skipDom = document.querySelector(".skip");
+// skipDom.addEventListener('click', function () {
+//     location.href = './register.html';
+// })
+// 获取节点
+let secondDom = document.querySelector(".second");
+let a = 5;
+let timeId = setInterval(function () {
+    a--;
+    secondDom.innerHTML = a;
+
+    if (a <= 0) {
+        clearInterval(timeId);
+        location.href = './register.html';
+    }
+}, 1000)
